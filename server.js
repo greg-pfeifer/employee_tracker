acf7e933d93f00f96400cd1bd90686c12763fa34
Greg P <gpfeif20@gmail.com>
2023-04-17 09:28:33 -0400

    added functions for add dept, role and emp, tested

--- server.js
+++ server.js
@@ -72,3 +72,96 @@ function viewAllEmps() {
         start()
     })
 }
+
+function addDept() {
+    inquirer.prompt({
+        name: 'dept_add',
+        type: 'input',
+        message: 'What is the name of the department you would like to add?'
+    }).then(answer => {
+        const sql_input = `INSERT INTO department (name) VALUES (?)`;
+        const dept_input = [answer.dept_add]
+        db.query(sql_input, dept_input, (err, result) => {
+            if (err)
+                throw err
+            console.log('Department has been added, see below.')
+            db.query(`SELECT * FROM department`, (err, result) => {
+                if (err)
+                    throw err
+                console.log(result)
+            })
+            start()
+        })
+    }
+    )}
+    
+function addRole() {
+    inquirer.prompt([
+        {
+            name: 'role_add_title',
+            type: 'input',
+            message: 'What is the name of the role you would like to add?'
+        },
+        {
+            name: 'role_add_salary',
+            type: 'input',
+            message: 'What is the salary of the role you would like to add?'
+        },
+        {
+            name: 'role_add_dept',
+            type: 'input',
+            message: 'Please enter the department ID to which this role corresponds.'
+        },
+    ]).then((answer) => {
+        const sql_input = `INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`;
+        const role_input = [answer.role_add_title, answer.role_add_salary, answer.role_add_dept]
+        db.query(sql_input, role_input, (err, result) => {
+            if (err)
+                throw err
+            console.log('ROLE HAS BEEN ADDED - SEE BELOW')
+            db.query(`SELECT * FROM role`, (err, result) => {
+                if (err)
+                    throw err
+                console.table(result)
+            })
+        })
+    })
+}
+
+function addEmp() {
+    inquirer.prompt([
+        {
+            name: 'emp_add_fn',
+            type: 'input',
+            message: 'What is the first name of the employee?'
+        },
+        {
+            name: 'emp_add_ln',
+            type: 'input',
+            message: 'What is the last name of the employee?'
+        },
+        {
+            name: 'emp_add_role',
+            type: 'input',
+            message: 'What is the role ID of the employee?'
+        },
+        {
+            name: 'emp_add_mgr',
+            type: 'input',
+            message: 'What is the ID of the manager of the employee?'
+        },
+    ]).then((answer) => {
+        const sql_input = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`;
+        const role_input = [answer.emp_add_fn, answer.emp_add_ln, answer.emp_add_role, answer.emp_add_mgr]
+        db.query(sql_input, role_input, (err, result) => {
+            if (err)
+                throw err
+            console.log('EMPLOYEE HAS BEEN ADDED - SEE BELOW')
+            db.query(`SELECT * FROM employee`, (err, result) => {
+                if (err)
+                    throw err
+                console.table(result)
+            })
+        })
+    })
+}
